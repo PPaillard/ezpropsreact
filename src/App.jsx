37f4bcc436c42e88d0ccae868a4cbc6edaae530a
PierreP <paillard.pierre@gmail.com>
2023-03-31 11:44:41 +0200

    Props version React

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-// j'appelle l'import comment je veux, c'est une fonction que je recup
-import articleFunction from "./components/Article";
+// j'appelle l'import en Pascal Case, c'est un composant (ça reste une fonction, mais c'est un composant au sens React du terme)
+import Article from "./components/Article";
 
 function App() {
   return (
     <div className="App">
-      {articleFunction("Mon Titre", "Mon super contenu!")}
+      <Article title="Mon Titre" content="Mon super contenu!" />
     </div>
   );
 }
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,8 +1,9 @@
-const Article = (title, content) => {
+// je récupère un "contenant", un objet dans lequel vont se trouver mes propriétés (props)
+const Article = (container) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <div>{content}</div>
+      <h1>{container.title}</h1>
+      <div>{container.content}</div>
     </div>
   );
 };
